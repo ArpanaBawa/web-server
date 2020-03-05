20c83606fadaead3f23e09d52ba2c5d11c1a2981
arpana bawa <abawa@tracfone.com>
2020-03-05 10:15:10 -0500

    updating source

--- middleware.js
+++ middleware.js
@@ -1,10 +1,14 @@
-var middleware={
-	requiredAuthentication: function(req, res, next){
-		console.log('private route hit');
+// Use module exports to expose middleware
+
+var middleware = {
+	requireAuthentication: function (req, res, next) {
+		console.log('private route hit!');
 		next();
 	},
-	logger: function(req, res, next){
-		console.log('Request: ' + new Date().toString() + ' ' + req.method + ' '  + req.originalUrl);
+	logger: function (req, res, next) {
+		console.log('Request: ' + new Date().toString() + ' ' + req.method + ' ' + req.originalUrl);
+		next();
 	}
 };
+
 module.exports = middleware;
